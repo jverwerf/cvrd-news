5498a6608b5146d5314e5c6afa9605e7c4bc0929
Jordy <verwerft@jordys-mac-mini.home>
2026-03-21 08:08:19 +0000

    Add Google AdSense: ad tile in Dashboard + verification script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&family=DM+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2572735826517528" crossOrigin="anonymous" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-594H3DW6ZD" />
         <script
           dangerouslySetInnerHTML={{
